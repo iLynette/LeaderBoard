c7325b188b75c41d4240be898f54d537e886dcc5
iLynette <lynetteacholah@icloud.com>
2022-03-02 18:26:51 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
 import './style.css';
-import fetchData from './modules/display';
-
+import fetchData from './modules/display.js';
 
 const refreshBtn = document.getElementById('refresh');
 const form = document.querySelector('form');
 const name = document.getElementById('userName');
 const score = document.getElementById('userScore');
-const id ='myId';
+const id = 'myId';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
 
-
 const apiHandler = () => {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        fetch(url, {
-         method: 'POST',
-         headers: {
-            'Content-type': 'application/json; charset= UTF-8',
-         },
-         body: JSON.stringify({
-             user: name.value,
-             score: score.value,
-        }),
-     });
-     form.reset();
-    })
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset= UTF-8',
+      },
+      body: JSON.stringify({
+        user: name.value,
+        score: score.value,
+      }),
+    });
+    form.reset();
+  });
 };
 
 apiHandler();
 
-
 refreshBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    fetchData();
+  e.preventDefault();
+  fetchData();
 });
 
 window.addEventListener('load', (e) => {
-    e.preventDefault();
-    fetchData();
+  e.preventDefault();
+  fetchData();
 });
